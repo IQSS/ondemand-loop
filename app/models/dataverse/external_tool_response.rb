5f853e3ac29077ea80a93ac966bbf11b30b6481d
David Verdú <davidverdu@theagilemonkeys.com>
2025-03-10 18:32:05 +0000

    refactor ExternalToolResponse as nested classes

--- app/models/dataverse/external_tool_response.rb
+++ app/models/dataverse/external_tool_response.rb
@@ -5,36 +5,36 @@ module Dataverse
     def initialize(json_str)
       parsed_data = JSON.parse(json_str)
       @status = parsed_data["status"]
-      @data = ExternalToolData.new(parsed_data["data"])
+      @data = Data.new(parsed_data["data"])
     end
-  end
 
-  class ExternalToolData
-    attr_accessor :query_parameters, :signed_urls
+    class Data
+      attr_accessor :query_parameters, :signed_urls
 
-    def initialize(data)
-      @query_parameters = ExternalToolQueryParameters.new(data["queryParameters"])
-      @signed_urls = data["signedUrls"].map { |url| ExternalToolSignedUrl.new(url) }
-    end
-  end
+      def initialize(data)
+        @query_parameters = QueryParameters.new(data["queryParameters"])
+        @signed_urls = data["signedUrls"].map { |url| SignedUrl.new(url) }
+      end
 
-  class ExternalToolQueryParameters
-    attr_accessor :dataset_pid, :dataset_id
+      class QueryParameters
+        attr_accessor :dataset_pid, :dataset_id
 
-    def initialize(data)
-      @dataset_pid = data["datasetPid"]
-      @dataset_id = data["datasetId"]
-    end
-  end
+        def initialize(data)
+          @dataset_pid = data["datasetPid"]
+          @dataset_id = data["datasetId"]
+        end
+      end
 
-  class ExternalToolSignedUrl
-    attr_accessor :name, :http_method, :signed_url, :time_out
+      class SignedUrl
+        attr_accessor :name, :http_method, :signed_url, :time_out
 
-    def initialize(data)
-      @name = data["name"]
-      @http_method = data["httpMethod"]
-      @signed_url = data["signedUrl"]
-      @time_out = data["timeOut"]
+        def initialize(data)
+          @name = data["name"]
+          @http_method = data["httpMethod"]
+          @signed_url = data["signedUrl"]
+          @time_out = data["timeOut"]
+        end
+      end
     end
   end
 end
