3c1db318f762e55c7c7129467544b1e9618e5dc6
David Verdú <davidverdu@theagilemonkeys.com>
2025-03-10 14:24:33 +0000

    refactor test

--- test/models/dataverse_metadata_test.rb
+++ test/models/dataverse_metadata_test.rb
@@ -12,38 +12,38 @@ class DataverseMetadataTest < ActiveSupport::TestCase
     FileUtils.remove_entry(@tmp_dir)
   end
 
-  test '#save and .find - saves a Dataverse Host to disk and retrieves it by id' do
+  test '#save and .find - saves a Dataverse dataverse_metadata to disk and retrieves it by id' do
     new_id = SecureRandom.uuid.to_s
-    host = DataverseMetadata.new
-    host.id = new_id
-    host.hostname = 'example.com'
-    host.port = 443
-    host.scheme = 'https'
-    host.save
+    dataverse_metadata = DataverseMetadata.new
+    dataverse_metadata.id = new_id
+    dataverse_metadata.hostname = 'example.com'
+    dataverse_metadata.port = 443
+    dataverse_metadata.scheme = 'https'
+    dataverse_metadata.save
 
-    retrieved_host = DataverseMetadata.find(new_id)
-    assert_not_nil retrieved_host
-    assert_equal 'example.com', retrieved_host.hostname
-    assert_equal 'https', retrieved_host.scheme
-    assert_equal new_id, retrieved_host.id
-    assert_equal 'https://example.com:443', retrieved_host.full_name
-    assert File.exist?(DataverseMetadata.filename_by_id(host.id))
+    retrieved_dataverse_metadata = DataverseMetadata.find(new_id)
+    assert_not_nil retrieved_dataverse_metadata
+    assert_equal 'example.com', retrieved_dataverse_metadata.hostname
+    assert_equal 'https', retrieved_dataverse_metadata.scheme
+    assert_equal new_id, retrieved_dataverse_metadata.id
+    assert_equal 'https://example.com:443', retrieved_dataverse_metadata.full_name
+    assert File.exist?(DataverseMetadata.filename_by_id(dataverse_metadata.id))
     assert_equal 1, Dir.glob(File.join(DataverseMetadata.metadata_directory, "*.yml")).count
     assert_equal 1, DataverseMetadata.all.count
   end
 
-  test '#save and .find - saves a Dataverse Host and fails to find another' do
+  test '#save and .find - saves a Dataverse dataverse_metadata and fails to find another' do
     new_id = SecureRandom.uuid.to_s
-    host = DataverseMetadata.new
-    host.id = new_id
-    host.hostname = 'example.com'
-    host.port = 443
-    host.scheme = 'https'
-    host.save
+    dataverse_metadata = DataverseMetadata.new
+    dataverse_metadata.id = new_id
+    dataverse_metadata.hostname = 'example.com'
+    dataverse_metadata.port = 443
+    dataverse_metadata.scheme = 'https'
+    dataverse_metadata.save
 
-    retrieved_host = DataverseMetadata.find(SecureRandom.uuid.to_s)
-    assert_nil retrieved_host
-    assert File.exist?(DataverseMetadata.filename_by_id(host.id))
+    retrieved_dataverse_metadata = DataverseMetadata.find(SecureRandom.uuid.to_s)
+    assert_nil retrieved_dataverse_metadata
+    assert File.exist?(DataverseMetadata.filename_by_id(dataverse_metadata.id))
     assert_equal 1, Dir.glob(File.join(DataverseMetadata.metadata_directory, "*.yml")).count
     assert_equal 1, DataverseMetadata.all.count
 
@@ -54,76 +54,76 @@ class DataverseMetadataTest < ActiveSupport::TestCase
     assert_equal 0, Dir.glob(File.join(DataverseMetadata.metadata_directory, "*.yml")).count
   end
 
-  test '.all - returns all saved hosts' do
-    host1 = DataverseMetadata.new.tap do |h|
+  test '.all - returns all saved dataverse_metadatas' do
+    dataverse_metadata1 = DataverseMetadata.new.tap do |h|
       h.id = SecureRandom.uuid.to_s
-      h.hostname = 'host1.com'
+      h.hostname = 'dataverse_metadata1.com'
       h.port = 80
       h.scheme = 'http'
     end
-    host1.save
+    dataverse_metadata1.save
 
-    host2 = DataverseMetadata.new.tap do |h|
+    dataverse_metadata2 = DataverseMetadata.new.tap do |h|
       h.id = SecureRandom.uuid.to_s
-      h.hostname = 'host2.com'
+      h.hostname = 'dataverse_metadata2.com'
       h.port = 443
       h.scheme = 'https'
     end
-    host2.save
+    dataverse_metadata2.save
 
-    all_hosts = DataverseMetadata.all
-    assert_equal 2, all_hosts.count
-    full_names = all_hosts.map(&:full_name)
-    assert_includes full_names, 'http://host1.com:80'
-    assert_includes full_names, 'https://host2.com:443'
+    all_dataverse_metadatas = DataverseMetadata.all
+    assert_equal 2, all_dataverse_metadatas.count
+    full_names = all_dataverse_metadatas.map(&:full_name)
+    assert_includes full_names, 'http://dataverse_metadata1.com:80'
+    assert_includes full_names, 'https://dataverse_metadata2.com:443'
     assert_equal 2, Dir.glob(File.join(DataverseMetadata.metadata_directory, "*.yml")).count
   end
 
-  test '.find_by_uri - finds a host by its URI' do
-    host = DataverseMetadata.new.tap do |h|
+  test '.find_by_uri - finds a dataverse_metadata by its URI' do
+    dataverse_metadata = DataverseMetadata.new.tap do |h|
       h.id = SecureRandom.uuid.to_s
       h.hostname = 'example.com'
       h.port = 443
       h.scheme = 'https'
     end
-    host.save
+    dataverse_metadata.save
 
-    found_host = DataverseMetadata.find_by_uri(@sample_uri)
-    assert_not_nil found_host
-    assert_equal 'https://example.com:443', found_host.full_name
+    found_dataverse_metadata = DataverseMetadata.find_by_uri(@sample_uri)
+    assert_not_nil found_dataverse_metadata
+    assert_equal 'https://example.com:443', found_dataverse_metadata.full_name
     assert_equal 1, Dir.glob(File.join(DataverseMetadata.metadata_directory, "*.yml")).count
   end
 
-  test '.find_by_uri - does not find a host for another URI' do
-    host = DataverseMetadata.new.tap do |h|
+  test '.find_by_uri - does not find a dataverse_metadata for another URI' do
+    dataverse_metadata = DataverseMetadata.new.tap do |h|
       h.id = SecureRandom.uuid.to_s
       h.hostname = 'example.com'
       h.port = 443
       h.scheme = 'https'
     end
-    host.save
+    dataverse_metadata.save
 
-    found_host = DataverseMetadata.find_by_uri(@another_sample_uri)
-    assert_nil found_host
+    found_dataverse_metadata = DataverseMetadata.find_by_uri(@another_sample_uri)
+    assert_nil found_dataverse_metadata
     assert_equal 1, Dir.glob(File.join(DataverseMetadata.metadata_directory, "*.yml")).count
   end
 
-  test '.find_or_initialize_by_uri - initializes a new host if none found' do
-    new_host = DataverseMetadata.find_or_initialize_by_uri(@sample_uri)
-    assert_not_nil new_host
-    assert_equal 'https://example.com:443', new_host.full_name
-    assert File.exist?(DataverseMetadata.filename_by_id(new_host.id))
+  test '.find_or_initialize_by_uri - initializes a new dataverse_metadata if none found' do
+    new_dataverse_metadata = DataverseMetadata.find_or_initialize_by_uri(@sample_uri)
+    assert_not_nil new_dataverse_metadata
+    assert_equal 'https://example.com:443', new_dataverse_metadata.full_name
+    assert File.exist?(DataverseMetadata.filename_by_id(new_dataverse_metadata.id))
     assert_equal 1, Dir.glob(File.join(DataverseMetadata.metadata_directory, "*.yml")).count
   end
 
-  test '.find_or_initialize_by_uri - does not create duplicate hosts' do
-    first_host = DataverseMetadata.find_or_initialize_by_uri(@sample_uri)
-    second_host = DataverseMetadata.find_or_initialize_by_uri(@sample_uri)
+  test '.find_or_initialize_by_uri - does not create duplicate dataverse_metadatas' do
+    first_dataverse_metadata = DataverseMetadata.find_or_initialize_by_uri(@sample_uri)
+    second_dataverse_metadata = DataverseMetadata.find_or_initialize_by_uri(@sample_uri)
 
-    assert_not_nil first_host
-    assert_not_nil second_host
-    assert_equal first_host.full_name, second_host.full_name
-    assert File.exist?(DataverseMetadata.filename_by_id(first_host.id))
+    assert_not_nil first_dataverse_metadata
+    assert_not_nil second_dataverse_metadata
+    assert_equal first_dataverse_metadata.full_name, second_dataverse_metadata.full_name
+    assert File.exist?(DataverseMetadata.filename_by_id(first_dataverse_metadata.id))
     assert_equal 1, Dir.glob(File.join(DataverseMetadata.metadata_directory, "*.yml")).count
     assert_equal 1, DataverseMetadata.all.count
   end
