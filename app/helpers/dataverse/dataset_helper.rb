86be1dd5983afe17a85cbd6d159d4a75c29e77d8
David Verdú <davidverdu@theagilemonkeys.com>
2025-03-13 12:29:21 +0000

    helper to see dataset file thumbnails

--- app/helpers/dataverse/dataset_helper.rb
+++ app/helpers/dataverse/dataset_helper.rb
@@ -1,2 +1,10 @@
 module Dataverse::DatasetHelper
+  def file_thumbnail(dataverse_metadata, file)
+    if ['image/png','image/jpeg', 'image/bmp', 'image/gif'].include? file.data_file.content_type
+      src = "#{dataverse_metadata.full_name}/api/access/datafile/#{file.data_file.id}?imageThumb=true"
+      image_tag(src, alt: file.label, title: file.label)
+    else
+      image_tag("file_thumbnail.png", alt: file.label, title: file.label)
+    end
+  end
 end
