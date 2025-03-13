0b4134226f938d53e1a1c179e33abc41e6377575
David Verdú <davidverdu@theagilemonkeys.com>
2025-03-13 12:29:58 +0000

    first draft of the dataset view

--- app/controllers/dataverse/dataset_controller.rb
+++ app/controllers/dataverse/dataset_controller.rb
@@ -1,7 +1,7 @@
 class Dataverse::DatasetController < ApplicationController
   def show
-    dataverse_metadata = Dataverse::DataverseMetadata.find(params[:metadata_id])
-    service = Dataverse::DataverseService.new(dataverse_metadata)
+    @dataverse_metadata = Dataverse::DataverseMetadata.find(params[:metadata_id])
+    service = Dataverse::DataverseService.new(@dataverse_metadata)
     @dataset = service.find_dataset_by_id(params[:id])
     @files = @dataset.data.latest_version.files
   end
