85bdc3e8419dca7aef6ee573f0a6d7a2211ce72e
David Verdú <davidverdu@theagilemonkeys.com>
2025-03-14 17:52:51 +0000

    enhance breadcrumbs and md5 label

--- app/helpers/dataverse/dataset_helper.rb
+++ app/helpers/dataverse/dataset_helper.rb
@@ -7,4 +7,22 @@ module Dataverse::DatasetHelper
       image_tag("file_thumbnail.png", alt: file.label, title: file.label)
     end
   end
+
+  def md5_with_copy(md5sum)
+    length = md5sum.length
+    truncated = "#{md5sum[0, 3]}...#{md5sum[length-3, 3]}"
+    #unique_id = "md5_#{SecureRandom.hex(4)}"
+
+    content_tag(:div, class: "d-inline-flex align-items-center gap-2") do
+      concat content_tag(:code, truncated, class: "text-monospace")
+      # concat button_tag(
+      #          "Copy",
+      #          type: "button",
+      #          class: "btn btn-sm btn-outline-secondary",
+      #          id: unique_id,
+      #          data: { clipboard_text: md5sum, bs_toggle: "tooltip", bs_title: "Copy to clipboard" },
+      #          onclick: "copyToClipboard(this);"
+      #        )
+    end
+  end
 end
