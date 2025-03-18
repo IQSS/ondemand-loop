d3074c527b1de85f68baff2e0ecda2ec8859f9ba
David Verdú <davidverdu@theagilemonkeys.com>
2025-03-18 16:09:45 +0000

    add error handling to DatasetsController

--- app/controllers/dataverse/datasets_controller.rb
+++ app/controllers/dataverse/datasets_controller.rb
@@ -3,10 +3,21 @@ class Dataverse::DatasetsController < ApplicationController
   before_action :find_dataset
 
   def show
-    @dataverse_metadata = Dataverse::DataverseMetadata.find(params[:metadata_id])
-    service = Dataverse::DataverseService.new(@dataverse_metadata)
-    @dataset = service.find_dataset_by_id(params[:id])
-    @files = @dataset.data.latest_version.files
+    begin
+      @dataverse_metadata = Dataverse::DataverseMetadata.find(params[:metadata_id])
+      unless @dataverse_metadata
+        flash[:error] = "Dataverse host metadata not found"
+        redirect_to root_path
+        return
+      end
+      service = Dataverse::DataverseService.new(@dataverse_metadata)
+      @dataset = service.find_dataset_by_id(params[:id])
+      @files = @dataset.data.latest_version.files
+    rescue Exception => e
+      Rails.logger.error("Dataverse service error: #{e.message}")
+      flash[:error] = "An error occurred while retrieving the dataset #{params[:id]}"
+      redirect_to root_path
+    end
   end
 
   def download
