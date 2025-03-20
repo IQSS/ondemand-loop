479240242299cb713dfa4c1cbc78e00c31ba5bbe
David Verdú <davidverdu@theagilemonkeys.com>
2025-03-20 17:51:02 +0000

    use before_action in controller

--- app/controllers/dataverse/datasets_controller.rb
+++ app/controllers/dataverse/datasets_controller.rb
@@ -1,16 +1,28 @@
 class Dataverse::DatasetsController < ApplicationController
+  before_action :find_dataverse_metadata
+  before_action :find_dataset
 
   def show
   end
 
   def download
+    @file_ids = params[:file_ids]
+    render json: @file_ids
+  end
+
+  private
+
+  def find_dataverse_metadata
+    @dataverse_metadata = Dataverse::DataverseMetadata.find(params[:metadata_id])
+    unless @dataverse_metadata
+      flash[:error] = "Dataverse host metadata not found"
+      redirect_to root_path
+      return
+    end
+  end
+
+  def find_dataset
     begin
-      @dataverse_metadata = Dataverse::DataverseMetadata.find(params[:metadata_id])
-      unless @dataverse_metadata
-        flash[:error] = "Dataverse host metadata not found"
-        redirect_to root_path
-        return
-      end
       service = Dataverse::DataverseService.new(@dataverse_metadata)
       @dataset = service.find_dataset_by_id(params[:id])
       unless @dataset
@@ -18,13 +30,11 @@ class Dataverse::DatasetsController < ApplicationController
         redirect_to root_path
         return
       end
-      @file_ids = params[:file_ids]
-      render json: @file_ids
-
     rescue Exception => e
       Rails.logger.error("Dataverse service error: #{e.message}")
       flash[:error] = "An error occurred while retrieving the dataset #{params[:id]}"
       redirect_to root_path
+      return
     end
   end
 end
