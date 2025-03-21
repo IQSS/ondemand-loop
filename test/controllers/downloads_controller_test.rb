55df374f222dab3f8f6b9bca9f0588240071c62f
David Verdú <davidverdu@theagilemonkeys.com>
2025-03-21 15:21:49 +0000

    get downloads in the controller

--- test/controllers/downloads_controller_test.rb
+++ test/controllers/downloads_controller_test.rb
@@ -1,6 +1,18 @@
 require "test_helper"
 
 class DownloadsControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @tmp_dir = Dir.mktmpdir
+    DownloadCollection.stubs(:metadata_root_directory).returns(@tmp_dir)
+    DownloadFile.stubs(:metadata_root_directory).returns(@tmp_dir)
+    Dataverse::DataverseMetadata.stubs(:metadata_root_directory).returns(@tmp_dir)
+  end
+
+  def teardown
+    FileUtils.rm_rf(@tmp_dir)
+  end
+
   test "should get index" do
     get downloads_url
     assert_response :success
