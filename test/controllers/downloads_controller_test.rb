a60d65c8d0e8587f2682a62a795ea97f38ffd085
David Verdú <davidverdu@theagilemonkeys.com>
2025-03-24 14:18:35 +0000

    add controller test

--- test/controllers/downloads_controller_test.rb
+++ test/controllers/downloads_controller_test.rb
@@ -7,14 +7,26 @@ class DownloadsControllerTest < ActionDispatch::IntegrationTest
     DownloadCollection.stubs(:metadata_root_directory).returns(@tmp_dir)
     DownloadFile.stubs(:metadata_root_directory).returns(@tmp_dir)
     Dataverse::DataverseMetadata.stubs(:metadata_root_directory).returns(@tmp_dir)
+    Rake.application.rake_require("tasks/populate")
+    Rake::Task.define_task(:environment)
   end
 
   def teardown
     FileUtils.rm_rf(@tmp_dir)
   end
 
-  test "should get index" do
+  test "should get index on empty disk" do
     get downloads_url
     assert_response :success
+    assert_select "div.col-md-9 > div.row", count: 0
+  end
+
+  test "should get index on disk with data" do
+    task = Rake::Task["dev:populate"]
+    task.invoke
+    get downloads_url
+    assert_response :success
+    assert_select "div.col-md-9 > div.row", count: 1
+    assert_select "div.col-md-9 > div.row > div.card > div.card", count: 4
   end
 end
