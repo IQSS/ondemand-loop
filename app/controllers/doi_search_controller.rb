f906410415d7978ec44f7378fb6b39c41c388dba
Aday Bujeda <aday@theagilemonkeys.com>
2025-04-09 11:55:51 +0100

    Moved DOI object url resolution to DOI Service

--- app/controllers/doi_search_controller.rb
+++ app/controllers/doi_search_controller.rb
@@ -13,9 +13,8 @@ class DoiSearchController < ApplicationController
       return
     end
 
-    doi_url =URI.parse("https://doi.org/#{doi}")
-    object_url = resolve(doi_url)
-
+    #TODO: Inject DOI Api url from configuration
+    object_url = Doi::DoiService.new.resolve(doi)
     if object_url.blank?
       flash[:error] = "Invalid DOI: #{doi}"
       redirect_to doi_search_path
@@ -25,6 +24,7 @@ class DoiSearchController < ApplicationController
     doi_resolver = Doi::DoiResolver.new(DoiResolversRegistry.resolvers)
     doi_info = doi_resolver.resolve(doi, object_url)
 
+    #TODO: This needs to be handled by a connector specific class
     if doi_info[:type] === 'dataverse'
       hostname = URI.parse(doi_info[:object_url]).hostname
       redirect_to view_dataverse_dataset_path(dv_hostname: hostname, persistent_id: doi)
@@ -32,21 +32,6 @@ class DoiSearchController < ApplicationController
       flash[:error] = "DOI not supported: #{doi} type: #{doi_info[:type]}"
       redirect_to doi_search_path
     end
-  end
-
-  private
-  def resolve(doi_url)
-    http_client = Common::HttpClient.new
-    response = http_client.head(doi_url.to_s)
-    if response.redirect?
-      object_url = response.location
-      log_info('DOI resolved', {doi_url: doi_url, object_url: object_url})
-      object_url
-    else
-      log_info('Unable to resolve DOI', {doi_url: doi_url, response: response.status})
-      nil
-    end
-
   end
 
 end
