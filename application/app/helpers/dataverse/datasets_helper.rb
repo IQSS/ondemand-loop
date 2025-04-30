dcab45c93a220d066f67bee8c272d99f64cb1362
David Verdú <davidverdu@theagilemonkeys.com>
2025-04-30 12:32:07 +0100

    enhance paginator helpers

--- application/app/helpers/dataverse/datasets_helper.rb
+++ application/app/helpers/dataverse/datasets_helper.rb
@@ -18,21 +18,23 @@ module Dataverse::DatasetsHelper
 
   def link_to_dataset_prev_page(dataverse_url, persistent_id, page, html_options = {})
     unless page.first_page?
+      uri = URI.parse(dataverse_url)
       url_options = {}
-      url_options[:dv_port] = params[:dv_port]
-      url_options[:dv_scheme] = params[:dv_scheme]
+      url_options[:dv_port] = uri.port if uri.port != 443
+      url_options[:dv_scheme] = uri.scheme if uri.scheme != 'https'
       url_options[:page] = page.prev_page
-      link_to("<", view_dataverse_dataset_path(URI.parse(dataverse_url).hostname, persistent_id, url_options), html_options)
+      link_to("<", view_dataverse_dataset_path(uri.hostname, persistent_id, url_options), html_options)
     end
   end
 
   def link_to_dataset_next_page(dataverse_url, persistent_id, page, html_options = {})
     unless page.last_page?
+      uri = URI.parse(dataverse_url)
       url_options = {}
-      url_options[:dv_port] = params[:dv_port]
-      url_options[:dv_scheme] = params[:dv_scheme]
+      url_options[:dv_port] = uri.port if uri.port != 443
+      url_options[:dv_scheme] = uri.scheme if uri.scheme != 'https'
       url_options[:page] = page.next_page
-      link_to(">", view_dataverse_dataset_path(URI.parse(dataverse_url).hostname, persistent_id, url_options), html_options)
+      link_to(">", view_dataverse_dataset_path(uri.hostname, persistent_id, url_options), html_options)
     end
   end
 
