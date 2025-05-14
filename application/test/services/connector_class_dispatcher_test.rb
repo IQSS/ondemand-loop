824238967fa646e6a250d9cd751e78c595f56201
David Verdú <davidverdu@theagilemonkeys.com>
2025-05-14 18:29:55 +0100

    fix test

--- application/test/services/connector_class_dispatcher_test.rb
+++ application/test/services/connector_class_dispatcher_test.rb
@@ -5,19 +5,19 @@ class ConnectorClassDispatcherTest < ActiveSupport::TestCase
 
   test 'file_connector_status should return Dataverse::ConnectorStatus class for dataverse files' do
     project = download_project(type: ConnectorType::DATAVERSE, files: 1)
-    result = ConnectorClassDispatcher.download_connector_status(project.files.first)
+    result = ConnectorClassDispatcher.download_connector_status(project.download_files.first)
     assert_instance_of Dataverse::DownloadConnectorStatus, result
   end
 
   test 'connector_metadata should return Dataverse::ConnectorMetadata class for dataverse files' do
     project = download_project(type: ConnectorType::DATAVERSE, files: 1)
-    result = ConnectorClassDispatcher.connector_metadata(project.files.first)
+    result = ConnectorClassDispatcher.connector_metadata(project.download_files.first)
     assert_instance_of Dataverse::DownloadConnectorMetadata, result
   end
 
   test 'download_processor should return Dataverse::ConnectorDownloadProcessor class for dataverse files' do
     project = download_project(type: ConnectorType::DATAVERSE, files: 1)
-    result = ConnectorClassDispatcher.download_processor(project.files.first)
+    result = ConnectorClassDispatcher.download_processor(project.download_files.first)
     assert_instance_of Dataverse::ConnectorDownloadProcessor, result
   end
 
