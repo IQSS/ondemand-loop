ed4c81aec5c33669a220f5fbb809837122f58ec6
David Verdú <davidverdu@theagilemonkeys.com>
2025-05-16 14:41:11 +0100

    both routes coexist

--- application/config/routes.rb
+++ application/config/routes.rb
@@ -5,10 +5,20 @@ Rails.application.routes.draw do
   get "uploads" => "uploads#index", as: :uploads
   get "uploads/files" => "uploads#files", as: :uploads_files
 
+  #old routes
+  post "downloads/:project_id/:file_id/cancel" => "download_files#cancel", as: :downloads_file_cancel
+  delete "downloads/:project_id/:file_id" => "download_files#destroy", as: :downloads_file_delete
+  post "uploads/:project_id/:collection_id/:file_id/cancel" => "upload_files#cancel", as: :uploads_file_cancel
+  post "uploads/:project_id/:collection_id/add" => "upload_files#add", as: :uploads_file_add
+  get "uploads/:project_id/:collection_id/files" => "upload_files#files", as: :uploads_file_files
+  delete "uploads/:project_id/:collection_id/:file_id" => "upload_files#delete_file", as: :uploads_file_delete
+  post "uploads/:project_id/create" => "upload_collections#create", as: :uploads_file_create_collection
+  post "/projects/:id/set_active" => "projects#set_active", as: :project_set_active
+
   # CRUD over /projects and /projects/:id
   # post /projects/:id/set_active => set project as active
   resources :projects do
-    post "set_active" => "projects#set_active", on: :member, as: :project_set_active
+    post "set_active" => "projects#set_active", on: :member
 
     # post /projects/:project_id/download_files/:id/cancel => cancel download file
     # delete /projects/:project_id/download_files/:id => delete download file
