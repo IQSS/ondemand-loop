1e83cb9d5867f41895637667f136db3247be689b
David Verdú <davidverdu@theagilemonkeys.com>
2025-05-22 15:57:17 +0100

    move method

--- application/app/services/dataverse/dataset_service.rb
+++ application/app/services/dataverse/dataset_service.rb
@@ -10,14 +10,5 @@ module Dataverse
       @http_client = http_client
       @file_utils = file_utils
     end
-
-    def get_citation_metadata
-      url = "/api/metadatablocks/citation"
-      response = @http_client.get(url)
-      return nil if response.not_found?
-      raise UnauthorizedException if response.unauthorized?
-      raise "Error getting dataverse subjects: #{response.status} - #{response.body}" unless response.success?
-      CitationMetadataResponse.new(response.body)
-    end
   end
 end
