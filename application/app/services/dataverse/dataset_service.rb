7a1955e17f0458a06fea399041a65c3dba495d8c
David Verdú <davidverdu@theagilemonkeys.com>
2025-05-23 11:13:16 +0100

    service method to create dataset

--- application/app/services/dataverse/dataset_service.rb
+++ application/app/services/dataverse/dataset_service.rb
@@ -3,12 +3,23 @@ module Dataverse
     include LoggingCommon
     include DateTimeCommon
 
+    AUTH_HEADER = 'X-Dataverse-key'
     class UnauthorizedException < Exception; end
 
-    def initialize(dataverse_url, http_client: Common::HttpClient.new(base_url: dataverse_url), file_utils: Common::FileUtils.new)
+    def initialize(dataverse_url, http_client: Common::HttpClient.new(base_url: dataverse_url), api_key:, file_utils: Common::FileUtils.new)
       @dataverse_url = dataverse_url
       @http_client = http_client
       @file_utils = file_utils
+      @api_key = api_key
+    end
+
+    def create_dataset(dataverse_id, body, api_key: @api_key)
+      url = "/api/dataverses/#{dataverse_id}/datasets"
+      response = @http_client.post(url, body: body.to_json, headers: { AUTH_HEADER => api_key })
+      return nil if response.not_found?
+      raise UnauthorizedException if response.unauthorized?
+      raise "Error creating dataset: #{response.status} - #{response.body}" unless response.success?
+      CreateDatasetResponse.new(response.body)
     end
   end
 end
