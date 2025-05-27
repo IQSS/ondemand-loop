a8d32eed5121a4bf8b457d456a39190e29f2741c
David Verdú <davidverdu@theagilemonkeys.com>
2025-05-27 11:26:27 +0100

    model test

--- application/app/models/dataverse/user_profile_response.rb
+++ application/app/models/dataverse/user_profile_response.rb
@@ -1,10 +1,11 @@
 module Dataverse
   class UserProfileResponse
-    attr_reader :status, :identifier, :display_name, :first_name, :last_name, :email, :superuser, :deactivated, :persistent_user_id
+    attr_reader :status, :id, :identifier, :display_name, :first_name, :last_name, :email, :superuser, :deactivated, :persistent_user_id
 
     def initialize(json)
       parsed = JSON.parse(json, symbolize_names: true)
       @status = parsed[:status]
+      @id = parsed.dig(:data, :id)
       @identifier = parsed.dig(:data, :identifier)
       @display_name = parsed.dig(:data, :displayName)
       @first_name = parsed.dig(:data, :firstName)
