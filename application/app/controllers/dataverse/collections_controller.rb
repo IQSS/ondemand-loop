ac39e56e3125dda4c700d3ca1b8bf9a38bfcc85f
David Verdú <davidverdu@theagilemonkeys.com>
2025-05-27 13:09:14 +0100

    create landing controller and views and translations

--- application/app/controllers/dataverse/collections_controller.rb
+++ application/app/controllers/dataverse/collections_controller.rb
@@ -2,19 +2,6 @@ class Dataverse::CollectionsController < ApplicationController
   include LoggingCommon
   include Dataverse::CommonHelper
 
-  def index
-    begin
-      hub_registry = DataverseHubRegistry.registry
-      installations = hub_registry.installations
-      page = params[:page] ? params[:page].to_i : 1
-      @installations_page = Page.new(installations, page, 25)
-    rescue Exception => e
-      log_error('Dataverse Installations service error', {}, e)
-      flash[:alert] = t(".dataverse_installations_service_error")
-      redirect_to root_path
-    end
-  end
-
   def show
     @dataverse_url = current_dataverse_url
     @service = Dataverse::CollectionService.new(@dataverse_url)
