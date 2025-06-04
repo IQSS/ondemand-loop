b682085ac69d4053715518c7972127036bf70e7e
Aday Bujeda <aday@theagilemonkeys.com>
2025-06-04 13:22:02 +0100

    Refactored deep link functionality + removed UploadFile.type + Fixed UploadFile partial path + Added remote dev build

---
+++ application/app/connectors/dataverse/actions/upload_batch_create.rb
@@ -0,0 +1,69 @@
+module Dataverse::Actions
+  class UploadBatchCreate
+    include LoggingCommon
+    include DateTimeCommon
+
+    def create(project, request_params)
+      remote_repo_url = request_params[:object_url]
+      dataverse_url = Dataverse::DataverseUrl.parse(remote_repo_url)
+
+      if dataverse_url.collection?
+        collection_service = Dataverse::CollectionService.new(dataverse_url.dataverse_url)
+        collection = collection_service.find_collection_by_id(dataverse_url.collection_id)
+        return error(I18n.t('connectors.dataverse.upload_batches.collection_not_found', url: remote_repo_url)) unless collection
+
+        root_dv = collection.data.parents.first
+        root_title = root_dv[:name]
+        collection_title = collection.data.name
+      elsif dataverse_url.dataset?
+        dataset_service = Dataverse::DatasetService.new(dataverse_url.dataverse_url)
+        dataset = dataset_service.find_dataset_version_by_persistent_id(dataverse_url.dataset_id)
+        return error(I18n.t('connectors.dataverse.upload_batches.dataset_not_found', url: remote_repo_url)) unless dataset
+
+        parent_dv = dataset.data.parents.last
+        root_dv = dataset.data.parents.first
+        root_title = root_dv[:name]
+        collection_title = parent_dv[:name]
+        dataset_title = dataset.metadata_field('title').to_s
+      else
+        collection_service = Dataverse::CollectionService.new(dataverse_url.dataverse_url)
+        collection = collection_service.find_collection_by_id(':root')
+        root_title = collection.data.name
+      end
+
+      file_utils = Common::FileUtils.new
+      upload_batch = UploadBatch.new.tap do |batch|
+        batch.id = file_utils.normalize_name(File.join(dataverse_url.domain, UploadBatch.generate_code))
+        batch.name = batch.id
+        batch.project_id = project.id
+        batch.remote_repo_url = remote_repo_url
+        batch.type = ConnectorType::DATAVERSE
+        batch.creation_date = now
+        batch.metadata = {
+          dataverse_url: dataverse_url.dataverse_url,
+          dataverse_title: root_title,
+          collection_title: collection_title,
+          dataset_title: dataset_title,
+          collection_id: dataverse_url.collection_id,
+          dataset_id: dataverse_url.dataset_id,
+        }
+      end
+      upload_batch.save
+
+      ConnectorResult.new(
+        resource: upload_batch,
+        message: { notice: I18n.t('connectors.dataverse.upload_batches.created', name: upload_batch.name) },
+        success: true
+      )
+    end
+
+    private
+
+    def error(message)
+      ConnectorResult.new(
+        message: { alert: message },
+        success: false
+      )
+    end
+  end
+end
