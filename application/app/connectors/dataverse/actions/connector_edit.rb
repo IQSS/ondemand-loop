4fd7fc5a0bbd7a8d9cdec3564b52b80c73e512eb
Aday Bujeda <aday@theagilemonkeys.com>
2025-06-20 09:44:09 +0100

    fix: Fixed API key collision with metadata method name

--- application/app/connectors/dataverse/actions/connector_edit.rb
+++ application/app/connectors/dataverse/actions/connector_edit.rb
@@ -10,13 +10,13 @@ module Dataverse::Actions
     def update(upload_bundle, request_params)
       repo_key = request_params[:api_key]
       scope = request_params[:key_scope]
-      if scope == 'collection'
+      if scope == 'bundle'
         metadata = upload_bundle.metadata
-        metadata[:api_key] = repo_key
+        metadata[:auth_key] = repo_key
         upload_bundle.update({ metadata: metadata })
       else
         server_domain = upload_bundle.connector_metadata.server_domain
-        RepoRegistry.repo_db.update(server_domain, metadata: {api_key: repo_key})
+        RepoRegistry.repo_db.update(server_domain, metadata: {auth_key: repo_key})
       end
 
       ConnectorResult.new(
