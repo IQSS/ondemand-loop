70175496a70e34750a29de81f50247d7a33d8f6f
David Verdú <davidverdu@theagilemonkeys.com>
2025-06-25 14:30:19 +0100

    make the Stimulus controller more generic

--- application/app/javascript/controllers/focus_toggle_controller.js
+++ application/app/javascript/controllers/focus_toggle_controller.js
@@ -1,10 +1,26 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-    focusInput() {
-        const input = document.querySelector("#input_repo_url")
-        if (input) {
-            input.focus()
+    static values = {
+        event: String,
+        elementId: String
+    }
+
+    connect() {
+        if (this.eventValue) {
+            this.element.addEventListener(this.eventValue, this.focusInput)
         }
+    }
+
+    disconnect() {
+        if (this.eventValue) {
+            this.element.removeEventListener(this.eventValue, this.focusInput)
+        }
+    }
+
+    focusInput = () => {
+        if (!this.elementIdValue) return
+        const target = document.getElementById(this.elementIdValue)
+        if (target) target.focus()
     }
 }
