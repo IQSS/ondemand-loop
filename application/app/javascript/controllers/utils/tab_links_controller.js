1a6f5b13e0ef34649bceb7335d91d729753df3ae
Aday Bujeda <aday@theagilemonkeys.com>
2025-06-26 10:43:52 +0100

    feat: UI improvements with scrollable tabs to avoid line breaks

--- application/app/javascript/controllers/utils/tab_links_controller.js
+++ application/app/javascript/controllers/utils/tab_links_controller.js
@@ -6,6 +6,7 @@ export default class extends Controller {
         if (hash) {
             const tabTriggerEl = document.querySelector(`a[href="${hash}"]`)
             if (tabTriggerEl && window.bootstrap?.Tab) {
+                this.scrollActiveTabIntoView()
                 const tab = new window.bootstrap.Tab(tabTriggerEl)
                 tab.show()
             }
@@ -19,5 +20,23 @@ export default class extends Controller {
                 }
             })
         })
+    }
+
+    scrollActiveTabIntoView() {
+        const activeTab = this.element.querySelector('.nav-link.active')
+        const scrollContainer = this.element.querySelector('.nav-tabs-wrapper')
+
+        if (activeTab && scrollContainer) {
+            // Only scroll if it's not fully visible
+            const tabRect = activeTab.getBoundingClientRect()
+            const containerRect = scrollContainer.getBoundingClientRect()
+
+            console.log(tabRect)
+            console.log(containerRect)
+            if (tabRect.left < containerRect.left || tabRect.right > containerRect.right) {
+                console.log('SCROLL')
+                activeTab.scrollIntoView({ behavior: 'smooth', inline: 'center' })
+            }
+        }
     }
 }
