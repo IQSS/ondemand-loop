405b091544e1cd86af6c87cf25db6fa42a6c5e5a
Aday Bujeda <aday@theagilemonkeys.com>
2025-08-28 16:06:47 +0100

    fixed translations for reset partial

--- application/test/services/reset_service_test.rb
+++ application/test/services/reset_service_test.rb
@@ -4,7 +4,16 @@ require 'test_helper'
 class ResetServiceTest < ActiveSupport::TestCase
   def setup
     @tmp_dir = Dir.mktmpdir
+    @lock_file = File.join(@tmp_dir, 'lock_file')
+    @socket_file = File.join(@tmp_dir, 'command.server.sock')
+
+    File.write(@lock_file, 'lock')
+    File.write(@socket_file, 'socket')
+
     Configuration.stubs(:metadata_root).returns(@tmp_dir)
+    Configuration.stubs(:detached_process_lock_file).returns(@lock_file)
+    Configuration.stubs(:command_server_socket_file).returns(@socket_file)
+
     FileUtils.mkdir_p(@tmp_dir)
   end
 
@@ -12,13 +21,19 @@ class ResetServiceTest < ActiveSupport::TestCase
     FileUtils.rm_rf(@tmp_dir) if Dir.exist?(@tmp_dir)
   end
 
-  test 'reset deletes metadata directory' do
+  test 'reset deletes metadata directory and files' do
     test_file = File.join(@tmp_dir, 'some_file')
     File.write(test_file, 'data')
 
     assert Dir.exist?(@tmp_dir)
+    assert File.exist?(@lock_file)
+    assert File.exist?(@socket_file)
+
     ResetService.new.reset
+
     refute Dir.exist?(@tmp_dir)
+    refute File.exist?(@lock_file)
+    refute File.exist?(@socket_file)
   end
 
   test 'logs error when deletion fails' do
@@ -32,6 +47,6 @@ class ResetServiceTest < ActiveSupport::TestCase
 
     assert Dir.exist?(@tmp_dir)
     assert_equal 1, reset_service.logged_messages.size
-    assert_match 'Failed to delete metadata directory', reset_service.logged_messages.first[:message]
+    assert_match 'Failed to reset application state', reset_service.logged_messages.first[:message]
   end
 end
