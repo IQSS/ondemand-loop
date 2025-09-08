964080e64443c94ac86f465dc7a94fde967441e1
Aday Bujeda <aday@theagilemonkeys.com>
2025-09-08 21:25:04 +0100

    Improvements

--- application/test/views/widgets/repository_activity_view_test.rb
+++ application/test/views/widgets/repository_activity_view_test.rb
@@ -3,6 +3,21 @@
 require 'test_helper'
 
 class RepositoryActivityViewTest < ActionView::TestCase
+
+  test 'renders only global tab when project_id not provided' do
+    service = mock('service')
+    service.stubs(:global).returns([])
+    service.stubs(:project_downloads).with(nil).returns([])
+    Repo::RepoActivityService.stubs(:new).returns(service)
+    view.stubs(:connector_icon).returns('')
+    view.stubs(:params).returns(ActionController::Parameters.new)
+
+    html = render partial: 'widgets/repository_activity'
+
+    refute_includes html, I18n.t('widgets.repository_activity.tab_project_label')
+    refute_includes html, connector_project_upload_bundles_path(project_id: '1')
+  end
+
   test 'renders project download urls tab when project_id provided' do
     project_id = '123'
 
